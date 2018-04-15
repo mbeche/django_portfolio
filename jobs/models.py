8710fdc02251013e9bb0f47aa003aa9ce69cef37
Mike <mb.johnsonbece@gmail.com>
2018-04-15 11:16:48 -0500

    Created model for jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model): #creates a new class in python, then saves it to the DB
+    image = models.ImageField(upload_to='images/') #see documentation: https://docs.djangoproject.com/en/2.0/ref/models/fields/#django.db.models.ImageField
+    summary = models.CharField(max_length=200) #CharField has a parameter: max_length
