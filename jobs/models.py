1cb872e644fdd28b33826799310fd0690b98e491
Mike <mb.johnsonbece@gmail.com>
2018-04-16 21:35:26 -0500

    Update model to add link

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Job(models.Model): #creates a new class in python, then saves it to the DB
     image = models.ImageField(upload_to='images/') #see documentation: https://docs.djangoproject.com/en/2.0/ref/models/fields/#django.db.models.ImageField
-    summary = models.CharField(max_length=200) #CharField has a parameter: max_length
+    summary = models.CharField(max_length=1000) #CharField has a parameter: max_length
+    link = models.URLField(max_length=200) #to make a link to the proper location
