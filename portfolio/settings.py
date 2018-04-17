1a1abf941eb880b139ba9f1d6ad097ffd11ee555
Mike <mb.johnsonbece@gmail.com>
2018-04-16 21:55:51 -0500

    Update the DB to "port" from "portfolioDB", re-migrated and was able to fix issue of not being able to create new jobs. Will have to research further how to edit model and database without breaking everything.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'NAME': 'port',
         'USER': 'postgres',
         'PASSWORD': 'django1234',
         'HOST': 'localhost',
